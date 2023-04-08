5e75ceb22bdb6f2c57b59db4fceeab15dcfbebf9
Salief <salieflewis@gmail.com>
2023-04-07 20:06:18 -0400

    add tailwind, initial layout styles, color configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,112 +1,66 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { GithubLogo, TwitterLogo, EnvelopeSimple } from 'phosphor-react';
+import Head from "next/head";
+
+export function Header() {
+  return (
+    <div className="flex justify-between my-4 text-sm">
+      <p>Salief Lewis</p>
+      <div className="flex gap-x-4">
+        <a
+          href="https://github.com/salieflewis"
+          target="_blank"
+          className="text-american-silver hover:text-white"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://twitter.com/salieflewis"
+          target="_blank"
+          className="text-american-silver hover:text-white"
+        >
+          Twitter
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Currently() {
+  return (
+    // bg-dark-charcoal
+    <div className="w-full  p-4 rounded-md text-center border border-american-silver hover:border-raisin-black hover:bg-american-silver hover:text-raisin-black">
+      <span className="text-lg">Core Contributor at Public Assembly</span>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div className="grid grid-cols-2">
+      <button>
+        <a href="http://recenteditions.app/" target="_blank">
+          <h3>Recent Editions &rarr;</h3>
+          <p>Frontend</p>
+        </a>
+      </button>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="mx-auto max-w-3xl">
+      {/* Head */}
       <Head>
         <title>Salief Lewis</title>
-        <meta name='description' content='Design Engineer' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Product Engineer" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
-      <main className={styles.main}>
-        <div className={styles.grid}>
-          <a
-            href='https://twitter.com/recenteditions'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Recent Editions Bot &rarr;</h3>
-            <p>Node.js</p>
-          </a>
-
-          <a
-            href='https://public---assembly.com/'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Public Assembly &rarr;</h3>
-            <p>Design / Engineering</p>
-          </a>
-
-          <a
-            href='http://recenteditions.app/'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Recent Editions &rarr;</h3>
-            <p>Frontend</p>
-          </a>
-
-          <a
-            href='https://magenta-hydrangeas.vercel.app/'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Magenta Hydrangeas &rarr;</h3>
-            <p>Frontend / Photography</p>
-          </a>
-
-          <a
-            href='https://ethglobal.com/showcase/release-club-ymdy3'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Release Club &rarr;</h3>
-            <p>Hackathon: Frontend</p>
-          </a>
-
-          <a
-            href='https://mirror.xyz/salief.eth/gIAHwG9qR3SiDz0QJdCZaBw9uCpdngAFuOV4ndtAiko'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>ZSK Redesign &rarr;</h3>
-            <p>Product Design / Writing</p>
-          </a>
-
-          <a
-            href='https://render-hack.vercel.app/'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Zora API Hack &rarr;</h3>
-            <p>Hackathon: Frontend</p>
-          </a>
-
-          <a
-            href='https://drive.google.com/file/d/1J2vvOuAengRvRxwKj2-SrJb33mVc98Ln/view'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>143 Washington &rarr;</h3>
-            <p>Adaptive Reuse</p>
-          </a>
-
-          <a
-            href='https://salieflewis.notion.site/salieflewis/Connect-Wallet-Design-Exploration-e720b5c4859049cda1e998550224af80'
-            target='_blank'
-            className={styles.card}
-          >
-            <h3>Connect Wallet Design &rarr;</h3>
-            <p>Product Design / Writing</p>
-          </a>
-        </div>
+      <main className="flex flex-col gap-y-8">
+        {/* Currently */}
+        <Currently />
       </main>
-
-      <footer className={styles.footer}>
-        <a href='https://github.com/salieflewis' target='_blank'>
-          <GithubLogo size={24} />
-        </a>
-        <a href='https://twitter.com/salieflewis' target='_blank'>
-          <TwitterLogo size={24} />
-        </a>
-        <a href='mailto:salieflewis@gmail.com' target='_blank'>
-          <EnvelopeSimple size={24} />
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ pages/writing.tsx
@@ -0,0 +1,29 @@
+{
+  /* <a
+  href="https://mirror.xyz/salief.eth/gIAHwG9qR3SiDz0QJdCZaBw9uCpdngAFuOV4ndtAiko"
+  target="_blank"
+>
+  <h3>ZSK Redesign &rarr;</h3>
+  <p>Product Design / Writing</p>
+</a>; */
+}
+
+{
+  /* <a
+href="https://drive.google.com/file/d/1J2vvOuAengRvRxwKj2-SrJb33mVc98Ln/view"
+target="_blank"
+>
+<h3>143 Washington &rarr;</h3>
+<p>Adaptive Reuse</p>
+</a> */
+}
+
+{
+  /* <a
+href="https://salieflewis.notion.site/salieflewis/Connect-Wallet-Design-Exploration-e720b5c4859049cda1e998550224af80"
+target="_blank"
+>
+<h3>Connect Wallet Design &rarr;</h3>
+<p>Product Design / Writing</p>
+</a> */
+}
